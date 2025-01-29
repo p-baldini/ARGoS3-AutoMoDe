3ab3fcb2ae3ded7e80e55374e9ae6661b4f09dfc
Paolo Baldini <paolo.baldini.phd@gmail.com>
2025-01-29 18:43:41 +0100

    corrected bug in repair evaluator

--- src/evaluators/AutoMoDeEvaluatorRepair.cpp
+++ src/evaluators/AutoMoDeEvaluatorRepair.cpp
@@ -41,42 +41,48 @@ namespace argos {
         std::vector<CCI_EPuckRangeAndBearingSensor::SReceivedPacket*> messages =
             m_pcRobotDAO->GetRangeAndBearingMessages();
 
-        // sort the messages according to sender and message ID
-        auto Comparator = [](auto a, auto b){
-            return a->Data[0] != b->Data[0] // check the sender of the two messages...
-            ? a->Data[0] < b->Data[0]       // ...if it differs, use it to compare the messages
-            : a->Data[1] < b->Data[1];      // ...if it coincides, compare according to message ID
+        // create and fill a vector where to save sender and content of a message
+        std::vector<std::pair<UInt8, UInt32>> senderAndContent;
+        for (auto m : messages) {
+            auto sender =  *((UInt32*)m->Data) >> 24;
+            auto content = *((UInt32*)m->Data) & 0x00ffffff;
+            senderAndContent.push_back(std::pair<UInt8, UInt32>(sender, content));
+        }
+
+        // sort the messages according to sender and message ID (sender has the precedence)
+        auto Comparator = [](auto a, auto b) {
+            return a.first != b.first ? a.first < b.first : a.second < b.second;
         };
-        std::sort(messages.begin(), messages.end(), Comparator);
+        std::sort(senderAndContent.begin(), senderAndContent.end(), Comparator);
 
         // remove messages with the same sender: we want only the last message
-        auto SameSource = [this](auto a, auto b){ return a->Data[0] == b->Data[0]; };
-        auto end = std::unique(messages.begin(), messages.end(), SameSource);
-        messages.erase(end, messages.end());
+        auto SameSource = [this](auto a, auto b){ return a.first == b.first; };
+        auto end = std::unique(senderAndContent.begin(), senderAndContent.end(), SameSource);
+        senderAndContent.erase(end, senderAndContent.end());
 
         // remove message that were already evaluated (same or smaller message ID from same sender)
         auto AlreadyConsidered = [this](auto o){
-            return un_mMessageIDs.find(o->Data[0]) != un_mMessageIDs.end()  // if the map does not
-                                                                            // contain the ID then
-                                                                            // the message has not
-                                                                            // been evaluated
-                                                                            // before...
-                && un_mMessageIDs[o->Data[0]] >= *o->Data;                  // if there is track of
-                                                                            // a message from the
-                                                                            // same sender, check
-                                                                            // if the message is
-                                                                            // newer.
+            return un_mMessageIDs.find(o.first) != un_mMessageIDs.end() // if the map does not
+                                                                        // contain the ID then the
+                                                                        // message has not been
+                                                                        // evaluated before...
+                && un_mMessageIDs[o.first] >= o.second;                 // if there is track of a
+                                                                        // message from the same
+                                                                        // sender, check if the
+                                                                        // message is newer.
         };
-        end = std::remove_if(messages.begin(), messages.end(), AlreadyConsidered);
-        messages.erase(end, messages.end());
+        end = std::remove_if(senderAndContent.begin(), senderAndContent.end(), AlreadyConsidered);
+        senderAndContent.erase(end, senderAndContent.end());
+
+        // std::cout << "aaa " << std::distance(messages.begin(), messages.end()) << std::endl;
 
         // count the new reward messages and add them to the performance
-        m_fCurrentPerformance = std::distance(messages.begin(), messages.end());
+        m_fCurrentPerformance = std::distance(senderAndContent.begin(), senderAndContent.end());
         m_fCumulativePerformance += m_fCurrentPerformance;
 
         // update the message IDs
-        for (auto o : messages) {
-            un_mMessageIDs[o->Data[0]] = *o->Data;
+        for (auto o : senderAndContent) {
+            un_mMessageIDs[o.first] = o.second;
         }
     }
 
