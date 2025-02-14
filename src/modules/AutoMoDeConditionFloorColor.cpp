cb968d9bde7511250743274423e9c2d91e4d8846
Paolo Baldini <paolo.baldini.phd@gmail.com>
2025-02-14 15:39:16 +0100

    lesser changes

--- src/modules/AutoMoDeConditionFloorColor.cpp
+++ src/modules/AutoMoDeConditionFloorColor.cpp
@@ -11,64 +11,64 @@
 
 namespace argos {
 
-    /****************************************/
-    /****************************************/
+	/****************************************/
+	/****************************************/
 
-    AutoMoDeConditionFloorColor::AutoMoDeConditionFloorColor() {
-        m_strLabel = "FloorColor";
-    }
+	AutoMoDeConditionFloorColor::AutoMoDeConditionFloorColor() {
+		m_strLabel = "FloorColor";
+	}
 
-    /****************************************/
-    /****************************************/
+	/****************************************/
+	/****************************************/
 
-    AutoMoDeConditionFloorColor::AutoMoDeConditionFloorColor(AutoMoDeConditionFloorColor* pc_condition) {
-        m_strLabel = pc_condition->GetLabel();
-        m_unIndex = pc_condition->GetIndex();
-        m_unIdentifier = pc_condition->GetIndex();
-        m_unFromBehaviourIndex = pc_condition->GetOrigin();
-        m_unToBehaviourIndex = pc_condition->GetExtremity();
-        m_mapParameters = pc_condition->GetParameters();
-        Init();
-    }
+	AutoMoDeConditionFloorColor::AutoMoDeConditionFloorColor(AutoMoDeConditionFloorColor* pc_condition) {
+		m_strLabel = pc_condition->GetLabel();
+		m_unIndex = pc_condition->GetIndex();
+		m_unIdentifier = pc_condition->GetIndex();
+		m_unFromBehaviourIndex = pc_condition->GetOrigin();
+		m_unToBehaviourIndex = pc_condition->GetExtremity();
+		m_mapParameters = pc_condition->GetParameters();
+		Init();
+	}
 
-    /****************************************/
-    /****************************************/
+	/****************************************/
+	/****************************************/
 
-    AutoMoDeConditionFloorColor* AutoMoDeConditionFloorColor::Clone() {
-        return new AutoMoDeConditionFloorColor(this);
-    }
+	AutoMoDeConditionFloorColor* AutoMoDeConditionFloorColor::Clone() {
+		return new AutoMoDeConditionFloorColor(this);
+	}
 
-    /****************************************/
-    /****************************************/
+	/****************************************/
+	/****************************************/
 
-    bool AutoMoDeConditionFloorColor::Verify() {
-        Real reading = m_bBasicPerceptionCapabilities
-            ? m_pcRobotDAO->GetGroundInput().Center
-            : m_pcRobotDAO->GetGroundReading();
+	bool AutoMoDeConditionFloorColor::Verify() {
+		Real reading = m_bBasicPerceptionCapabilities
+			? m_pcRobotDAO->GetGroundInput().Center
+			: m_pcRobotDAO->GetGroundReading();
 
-        // check if the robot perceives a value in the surrounding of the desired value
-        bool perceived = (Real) m_fGroundColor - .25 <= reading && reading <= (Real) m_fGroundColor + .25;
+		// check if the robot perceives a value in the surrounding of the desired value
+		bool perceived = (Real) m_fGroundColor - .25 <= reading && reading <= (Real) m_fGroundColor + .25;
 
-        // if the condition type is `absence`, negate the presence of the perceived color
-        perceived = (UInt8) m_eType == 1 ? ! perceived : perceived;
+		// if the condition type is `absence`, negate the presence of the perceived color
+		perceived = (UInt8) m_eType == 1 ? ! perceived : perceived;
 
-        // if the ground condition is enabled, check if transition probability
-        return perceived && EvaluateBernoulliProbability(m_fProbability);
-    }
+		// if the ground condition is enabled, check if transition probability
+		return perceived && EvaluateBernoulliProbability(m_fProbability);
+	}
 
-    /****************************************/
-    /****************************************/
+	/****************************************/
+	/****************************************/
 
-    void AutoMoDeConditionFloorColor::Reset() {
-        Init();
-    }
+	void AutoMoDeConditionFloorColor::Reset() {
+		Init();
+	}
 
-    /****************************************/
-    /****************************************/
+	/****************************************/
+	/****************************************/
 
-    void AutoMoDeConditionFloorColor::Init() {
-        m_eType = FindParameter("t");
-        m_fGroundColor = FindParameter("v");
-        m_fProbability = FindParameter("p");
-    }
+	void AutoMoDeConditionFloorColor::Init() {
+		m_eType = FindParameter("t");
+		m_fGroundColor = FindParameter("v");
+		m_fProbability = FindParameter("p");
+	}
 }
