bcf91301eab0d42bfd3202e85e3753b24dd5826d
Paolo Baldini <paolo.baldini.phd@gmail.com>
2025-02-19 10:09:59 +0100

    added check in observable subscriber

--- src/core/AutoMoDeObservers.cpp
+++ src/core/AutoMoDeObservers.cpp
@@ -13,6 +13,8 @@ namespace argos {
 	/****************************************/
 
 	void AutoMoDeObservable::Unsubscribe(AutoMoDeObserver* observer, const std::string event) {
-		m_Observers[event].erase(observer);
+		if (m_Observers.find(event) != m_Observers.end()) {
+			m_Observers[event].erase(observer);
+		}
 	}
 }
